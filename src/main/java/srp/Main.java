f992206b25d9c89468d0c9eaf6f5163a7192aa0f
Gorky Palacios Mutis <gorky.palacios@paymentevolution>
2025-06-15 20:06:58 -0500

    feat: agregar implementación inicial de UserManager con violación del SRP

--- src/main/java/srp/Main.java
+++ src/main/java/srp/Main.java
@@ -1,17 +1,40 @@
 package srp;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.regex.Pattern;
+
+class UserManager {
+    public void addUser(String email, String password) {
+        if (isValidEmail(email) && isValidPassword(password)) {
+            saveToDatabase(email, password);
+            sendWelcomeEmail(email);
+        } else {
+            System.out.println("Invalid email or password. User not added.");
+        }
+    }
+
+    private boolean isValidEmail(String email) {
+        return Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", email);
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.length() >= 8;
+    }
+
+    private void saveToDatabase(String email, String password) {
+        System.out.println("Saving user to the database...");
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+    }
+
+    private void sendWelcomeEmail(String email) {
+        System.out.println("Sending welcome email to " + email);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        UserManager userManager = new UserManager();
+        userManager.addUser("example@domain.com", "password123");
+        userManager.addUser("invalid-email", "1234");
     }
 }
