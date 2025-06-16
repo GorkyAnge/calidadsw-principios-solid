6af55f6ad2e670d946faded6a5f11767f7b9347a
DDCT <dylanclerque_2003@hotmail.com>
2025-06-15 19:55:03 -0500

    Código a refactorizar

--- src/main/java/ocp/Main.java
+++ src/main/java/ocp/Main.java
@@ -1,17 +1,25 @@
 package ocp;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class NotificationService {
+    public void sendNotification(String type, String message) {
+        if (type.equals("Email")) {
+            System.out.println("Sending Email: " + message);
+        } else if (type.equals("SMS")) {
+            System.out.println("Sending SMS: " + message);
+        } else if (type.equals("Push")) {
+            System.out.println("Sending Push Notification: " + message);
+        } else {
+            System.out.println("Invalid notification type!");
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        NotificationService service = new NotificationService();
+        service.sendNotification("Email", "Hello via Email!");
+        service.sendNotification("SMS", "Hello via SMS!");
+        service.sendNotification("Push", "Hello via Push Notification!");
+        service.sendNotification("Fax", "Hello via Fax!");
     }
 }
